80236031b76376016eda4fd54eafb50344266a8e
Bruno Henc <hencb@protomail.ch>
2017-07-06 09:18:26 +0000

    added sync_branch_update()

--- pym/portage/sync/modules/git/git.py
+++ pym/portage/sync/modules/git/git.py
@@ -89,6 +89,44 @@ class GitSync(NewBase):
 			return (exitcode, False)
 		return (os.EX_OK, True)
 
+	def sync_branch_update(self):
+		'''switches a git repository branch to match the value set by sync-branch
+		if it is unable to switch to the correct branch it will stay at the old
+		branch'''
+		git_cmd = "cd %s && git branch --all" % self.repo.location
+		try:
+			rawbranch = subprocess.check_output(git_cmd, shell=True, \
+				universal_newlines=True)
+		except subprocess.CalledProcessError:
+			return os.EX_NOTFOUND, True
+		for branchline in rawbranch.split("\n"):
+			blist = branchline.strip().split()
+			if blist[0] == "*":
+				if blist[1] == self.repo.sync_branch:
+					return self.simple_update() #we're at the correct branch
+			elif "/" not in blist[0]:
+				if blist[0] == self.sync_branch: #branch exists locally, checkout
+					git_cmd = "cd %s && git checkout %s" % \
+						self.repo.location, self.repo.sync_branch
+					try:
+						subprocess.check_output(git_cmd, shell=True)
+					except subprocess.CalledProcessError:
+						return self.simple_update()
+					return self.simple_update()
+			elif not "HEAD" in blist[0] and "/" in blist[0]: #branch exists remotely
+				branchl = blist.split("/")
+				if branchl[2] == self.repo.sync_branch: #found the correct value
+					git_cmd = "cd %s && git checkout -b %s" % \
+						self.repo.location, self.repo.sync_branch
+					git_cmd += " --track origin/%s" % self.repo.sync_branch
+					try:
+						subprocess.check_output(git_cmd, shell=True)
+					except subprocess.CalledProcessError:
+						return self.simple_update()
+					return self.simple_update()
+		#There should be a big red warning here that the branch doesn't exist
+		#if we get to this point. Instead we'll just sync the branch for now
+		return self.simple_update()
 	def sync_branch_check(self):
 		'''checks that self.repo.sync_branch matches the output of git branch -v i.e.
 		checks if git and repos.conf agree on what the sync-branch value is
@@ -119,40 +157,45 @@ class GitSync(NewBase):
 			if rlist[0] == "origin" and rlist[2] == "(fetch)":
 				return self.repo.sync_uri == rlist[1]
 
-	def update(self):
-		''' Update existing git repository, and ignore the syncuri by default. We are
+	def simple_update(self):
+		''' Update existing git repository, and ignore the sync-uri by default. We are
 		going to trust the user and assume that the user is in the branch
 		that he/she wants updated. We'll let the user manage branches with
-		git directly. However, if auto-sync-enforcing = yes is set, portage will enforce
+		git directly.
+		'''
+		git_cmd_opts = ""
+		if self.settings.get("PORTAGE_QUIET") == "1":
+			git_cmd_opts += " --quiet"
+		if self.repo.module_specific_options.get('sync-git-pull-extra-opts'):
+			git_cmd_opts += " %s" % self.repo.module_specific_options['sync-git-pull-extra-opts']
+		git_cmd = "%s pull%s" % (self.bin_command, git_cmd_opts)
+		writemsg_level(git_cmd + "\n")
+
+		rev_cmd = [self.bin_command, "rev-list", "--max-count=1", "HEAD"]
+		previous_rev = subprocess.check_output(rev_cmd,
+			cwd=portage._unicode_encode(self.repo.location))
+
+		exitcode = portage.process.spawn_bash("cd %s ; exec %s" % (
+				portage._shell_quote(self.repo.location), git_cmd),
+			**self.spawn_kwargs)
+		if exitcode != os.EX_OK:
+			msg = "!!! git pull error in %s" % self.repo.location
+			self.logger(self.xterm_titles, msg)
+			writemsg_level(msg + "\n", level=logging.ERROR, noiselevel=-1)
+			return (exitcode, False)
+
+		current_rev = subprocess.check_output(rev_cmd,
+			cwd=portage._unicode_encode(self.repo.location))
+
+		return (os.EX_OK, current_rev != previous_rev)
+
+	def update(self):
+		'''However, if auto-sync-enforcing = yes is set, portage will enforce
 		that the values in the repos.conf files match the ones in the git repositories,
 		and will automagically update the git repositories.
 		'''
 		if self.repo.auto_sync_enforcing == "no" or self.repo.auto_sync_enforcing is None:
-			git_cmd_opts = ""
-			if self.settings.get("PORTAGE_QUIET") == "1":
-				git_cmd_opts += " --quiet"
-			if self.repo.module_specific_options.get('sync-git-pull-extra-opts'):
-				git_cmd_opts += " %s" % self.repo.module_specific_options['sync-git-pull-extra-opts']
-			git_cmd = "%s pull%s" % (self.bin_command, git_cmd_opts)
-			writemsg_level(git_cmd + "\n")
-
-			rev_cmd = [self.bin_command, "rev-list", "--max-count=1", "HEAD"]
-			previous_rev = subprocess.check_output(rev_cmd,
-				cwd=portage._unicode_encode(self.repo.location))
-
-			exitcode = portage.process.spawn_bash("cd %s ; exec %s" % (
-					portage._shell_quote(self.repo.location), git_cmd),
-				**self.spawn_kwargs)
-			if exitcode != os.EX_OK:
-				msg = "!!! git pull error in %s" % self.repo.location
-				self.logger(self.xterm_titles, msg)
-				writemsg_level(msg + "\n", level=logging.ERROR, noiselevel=-1)
-				return (exitcode, False)
-
-			current_rev = subprocess.check_output(rev_cmd,
-				cwd=portage._unicode_encode(self.repo.location))
-
-			return (os.EX_OK, current_rev != previous_rev)
+			return self.simple_update()
 		else:
 			#Check sync-uri first. If sync-uri doesn't match, nuke the git repository
 			#and start from scratch. This will automagically set the correct 
@@ -165,5 +208,7 @@ class GitSync(NewBase):
 			#switch to the correct branch. 
 			if self.repo.sync_branch is not None and self.repo.location is not None:
 				if not self.sync_branch_check():
-					print("sync-branch does not match")
-			return (os.EX_OK, True)
+					return self.sync_branch_update()
+				else:
+					return self.simple_update()
+			return self.simple_update()
